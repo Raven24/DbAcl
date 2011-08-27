e2edc99ce56537b2986996d21e06866e985924c5
Florian Staudacher <florian.staudacher@gmx.at>
2011-08-27 22:06:00 +0200

    removed pseudo authentication code, not needed currently

--- includes/auth.inc.php
+++ includes/auth.inc.php
@@ -4,25 +4,7 @@ session_start();
 
 // for now, just login without credentials
 // normally, you would have to validate a login somewhere around here
-if( page() == 'login' && $_GET['proceed'] == 1 )
-{
-    $_SESSION['loggedIn'] = 1;
-}
-
-// delete session
-if( page() == 'logout' )
-{
-    session_unset();
-    $_SESSION = array();
-
-}
-
-// if we are not logged in, redirect
-if( !loggedIn() && page() != 'login' && page() != 'logout' )
-{
-    redirect(array('page'=>'login'));
-}
-
+$_SESSION['loggedIn'] = 1;
 
 
 function loggedIn()
